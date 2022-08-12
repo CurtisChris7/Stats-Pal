1d551ff7ca2575a0dd18cfcac3178d0ee30ecc84
Christopher Curtis <curtischris7@gmail.com>
2022-08-12 13:28:46 -0400

    Made adjustments for first release

---
+++ unittest.py
@@ -0,0 +1,15 @@
+import unittest
+from UnitTests.ApproximateNormalTableTests import ApproximateNormalTableTests
+from UnitTests.ScipPyTDistributionTests import SciPyTDistributionTests
+from UnitTests.BinomialDistributionTests import BinomialDistributionTests
+from UnitTests.BootstrappedCentralValueAnalyzerTests import BootstrappedCentralValueAnalyzerTests
+from UnitTests.NormalCentralValueAnalyzerTests import NormalCentralValueAnalyzerTests
+from UnitTests.SciPyChiSquaredDistributionTests import SciPyChiSquaredTests
+from UnitTests.NormalBinomialAnalyzerTests import NormalBinomialAnalyzerTests
+from UnitTests.SampleUtilitiesTests import SampleUtilitiesTests
+from UnitTests.CategoricalSampleUtilitiesTests import CategoricalSampleUtilitiesTests
+from UnitTests.TDistributionCentralValueAnalyzerTests import TDistributionCentralValueAnalyzerTests
+from UnitTests.NormalVarianceAnalyzerTests import NormalVarianceAnalyzerTests
+
+if __name__ == "__main__":
+    unittest.main()
